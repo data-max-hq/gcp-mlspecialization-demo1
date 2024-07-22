e4c12e2775d8df024163a3acec27650217fcb78b
Erbli Kuka <42184398+ekuka86@users.noreply.github.com>
2024-07-23 00:13:07 +0200

    minor changes

--- components/data_transformation.py
+++ components/data_transformation.py
@@ -3,7 +3,7 @@ import tensorflow as tf
 import tensorflow_transform as tft
 from tfx.proto import transform_pb2
 
-# Define feature keys and label key
+
 _FEATURE_KEYS = [
     "TripSeconds", "TripMiles", "PickupCommunityArea", "DropoffCommunityArea", 
     "TripStartTimestamp", "TripEndTimestamp", "PaymentType", "Company"
@@ -12,6 +12,8 @@ _CATEGORICAL_NUMERICAL_FEATURES = ["PickupCommunityArea", "DropoffCommunityArea"
 _CATEGORICAL_STRING_FEATURES = ["TripStartTimestamp", "TripEndTimestamp", "PaymentType", "Company"]
 _NUMERIC_FEATURES = ["TripSeconds", "TripMiles"]
 _LABEL_KEY = 'Fare'
+_VOCAB_SIZE = 1000
+_OOV_SIZE = 10
 
 def t_name(key):
     """
@@ -24,24 +26,46 @@ def t_name(key):
     """
     return key + '_xf'
 
+
+def _make_one_hot(x, key):
+    """Make a one-hot tensor to encode categorical features.
+    Args:
+      x: A dense tensor
+      key: A string key for the feature in the input
+    Returns:
+      A dense one-hot tensor as a float list
+    """
+    integerized = tft.compute_and_apply_vocabulary(x,
+            top_k=_VOCAB_SIZE,
+            num_oov_buckets=_OOV_SIZE,
+            vocab_filename=key, name=key)
+    depth = (
+        tft.experimental.get_vocabulary_size_by_name(key) + _OOV_SIZE)
+    one_hot_encoded = tf.one_hot(
+        integerized,
+        depth=tf.cast(depth, tf.int32),
+        on_value=1.0,
+        off_value=0.0)
+    return tf.reshape(one_hot_encoded, [-1, depth])
+
 def preprocessing_fn(inputs):
     """Preprocess input columns into transformed columns."""
     outputs = {}
 
-    # Pass through categorical numerical features without transformation
+    # Pass through categorical numerical features with one-hot encoding
     for key in _CATEGORICAL_NUMERICAL_FEATURES:
-        outputs[t_name(key)] = inputs[key]
+        outputs[t_name(key)] = _make_one_hot(inputs[key], key)
 
-    # Pass through categorical string features without transformation
+    # Pass through categorical string features with one-hot encoding
     for key in _CATEGORICAL_STRING_FEATURES:
-        outputs[t_name(key)] = inputs[key]
+        outputs[t_name(key)] = _make_one_hot(inputs[key], key)
 
     # Scale numeric features
     for key in _NUMERIC_FEATURES:
-        outputs[t_name(key)] = inputs[key]
+        outputs[t_name(key)] = tft.scale_to_z_score(inputs[key])
 
     # Scale the label key
-    outputs[t_name(_LABEL_KEY)] = inputs[_LABEL_KEY]
+    outputs[t_name(_LABEL_KEY)] = tft.scale_to_z_score(inputs[_LABEL_KEY])
     
     return outputs
 
