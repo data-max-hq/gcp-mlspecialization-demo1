92037fb876ad536c8d4e8e6eabbfef5454261e46
Erbli Kuka <42184398+ekuka86@users.noreply.github.com>
2024-07-25 00:01:13 +0200

    minor changes

--- components/data_transformation.py
+++ components/data_transformation.py
@@ -1,24 +1,12 @@
-from tfx.components import Transform
 import tensorflow as tf
 import tensorflow_transform as tft
+from tfx.v1 import components
 from tfx.proto import transform_pb2
-
-# Define feature keys and label key
-_FEATURE_KEYS = [
-    "TripSeconds", "TripMiles", "PickupCommunityArea", "DropoffCommunityArea", 
-    "TripStartTimestamp", "TripEndTimestamp", "PaymentType", "Company"
-]
+from tfx.components import Transform
 
 NUMERICAL_FEATURES = ['TripSeconds', 'TripMiles']
-
-CATEGORICAL_NUMERICAL_FEATURES = [
-    'PickupCommunityArea', 'DropoffCommunityArea'
-]
-
-CATEGORICAL_STRING_FEATURES = [
-    'PaymentType', 'Company'
-]
-
+CATEGORICAL_NUMERICAL_FEATURES = ['PickupCommunityArea', 'DropoffCommunityArea']
+CATEGORICAL_STRING_FEATURES = ['PaymentType', 'Company']
 VOCAB_SIZE = 1000
 OOV_SIZE = 10
 LABEL_KEY = 'Fare'
@@ -31,8 +19,7 @@ def _make_one_hot(x, key):
             top_k=VOCAB_SIZE,
             num_oov_buckets=OOV_SIZE,
             vocab_filename=key, name=key)
-    depth = (
-        tft.experimental.get_vocabulary_size_by_name(key) + OOV_SIZE)
+    depth = tft.experimental.get_vocabulary_size_by_name(key) + OOV_SIZE
     one_hot_encoded = tf.one_hot(
         integerized,
         depth=tf.cast(depth, tf.int32),
@@ -44,14 +31,21 @@ def preprocessing_fn(inputs):
     outputs = {}
     for key in NUMERICAL_FEATURES:
         outputs[t_name(key)] = tft.scale_to_z_score(inputs[key], name=key)
-
     for key in CATEGORICAL_STRING_FEATURES:
         outputs[t_name(key)] = _make_one_hot(inputs[key], key)
-
     for key in CATEGORICAL_NUMERICAL_FEATURES:
         outputs[t_name(key)] = _make_one_hot(tf.strings.strip(
             tf.strings.as_string(inputs[key])), key)
-    
-    # Output the label
     outputs[LABEL_KEY] = inputs[LABEL_KEY]
     return outputs
+
+def create_transform(example_gen, schema_gen):
+    return Transform(
+        examples=example_gen.outputs['examples'],
+        schema=schema_gen.outputs['schema'],
+        module_file="components/data_transformation.py",
+        splits_config=transform_pb2.SplitsConfig(
+            analyze=['train'],
+            transform=['train', 'eval']
+        )
+    )
