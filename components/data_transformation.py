ec5bf9bba7bae239a70d5559b1744dc70f076702
Erbli Kuka <42184398+ekuka86@users.noreply.github.com>
2024-07-24 15:20:09 +0200

    minor changes

--- components/data_transformation.py
+++ components/data_transformation.py
@@ -4,44 +4,66 @@ import tensorflow_transform as tft
 from tfx.proto import transform_pb2
 
 # Define feature keys and label key
-_FEATURE_KEYS = [
-    "TripSeconds", "TripMiles", "PickupCommunityArea", "DropoffCommunityArea", 
-    "TripStartTimestamp", "TripEndTimestamp", "PaymentType", "Company"
-]
+_NUMERICAL_FEATURES = ["TripMiles", "TripSeconds"]
 _CATEGORICAL_NUMERICAL_FEATURES = ["PickupCommunityArea", "DropoffCommunityArea"]
 _CATEGORICAL_STRING_FEATURES = ["TripStartTimestamp", "TripEndTimestamp", "PaymentType", "Company"]
-_NUMERIC_FEATURES = ["TripSeconds", "TripMiles"]
+_BUCKET_FEATURES = ["PickupCommunityArea", "DropoffCommunityArea"]
 _LABEL_KEY = 'Fare'
+_FEATURE_BUCKET_COUNT = 10
+_VOCAB_SIZE = 1000
+_OOV_SIZE = 1
 
+# Utility function for renaming keys
 def t_name(key):
-    """
-    Rename the feature keys so that they don't clash with the raw keys when running the Evaluator component.
-    Args:
-      key: The original feature key
-    Returns:
-      key with '_xf' appended
-    """
     return key + '_xf'
 
+def _make_one_hot(x, key):
+    """Make a one-hot tensor to encode categorical features.
+    Args:
+        X: A dense tensor
+        key: A string key for the feature in the input
+    Returns:
+        A dense one-hot tensor as a float list
+    """
+    integerized = tft.compute_and_apply_vocabulary(
+        x,
+        top_k=_VOCAB_SIZE,
+        num_oov_buckets=_OOV_SIZE,
+        vocab_filename=key, name=key)
+    depth = tft.experimental.get_vocabulary_size_by_name(key) + _OOV_SIZE
+    one_hot_encoded = tf.one_hot(
+        integerized, depth=tf.cast(depth, tf.int32),
+        on_value=1.0, off_value=0.0)
+    return tf.reshape(one_hot_encoded, [-1, depth])
+
 def preprocessing_fn(inputs):
-    """Preprocess input columns into transformed columns."""
+    """tf.transform's callback function for preprocessing inputs.
+    Args:
+        inputs: map from feature keys to raw not-yet-transformed features.
+    Returns:
+        Map from string feature key to transformed feature operations.
+    """
     outputs = {}
+    
+    # Scale numerical features to z-score
+    for key in _NUMERICAL_FEATURES:
+        outputs[t_name(key)] = tft.scale_to_z_score(inputs[key], name=key)
 
-    # Pass through numeric features
-    for key in _NUMERIC_FEATURES:
-        outputs[t_name(key)] = inputs[key]
+    # Bucketize numerical features to categorical
+    for key in _BUCKET_FEATURES:
+        outputs[t_name(key)] = tf.cast(tft.bucketize(inputs[key], _FEATURE_BUCKET_COUNT, name=key), dtype=tf.float32)
 
-    # Pass through categorical numerical features
-    for key in _CATEGORICAL_NUMERICAL_FEATURES:
-        outputs[t_name(key)] = inputs[key]
-
-    # Pass through categorical string features
+    # Apply one-hot encoding to string categorical features
     for key in _CATEGORICAL_STRING_FEATURES:
-        outputs[t_name(key)] = inputs[key]
+        outputs[t_name(key)] = _make_one_hot(inputs[key], key)
+
+    # Apply one-hot encoding to numerical categorical features after converting them to string
+    for key in _CATEGORICAL_NUMERICAL_FEATURES:
+        outputs[t_name(key)] = _make_one_hot(tf.strings.strip(tf.strings.as_string(inputs[key])), key)
 
     # Pass through the label key
-    outputs[(_LABEL_KEY)] = inputs[_LABEL_KEY]
-    
+    outputs[_LABEL_KEY] = inputs[_LABEL_KEY]
+
     return outputs
 
 def create_transform(example_gen, schema_gen):
--- components/model_trainer.py
+++ components/model_trainer.py
@@ -9,7 +9,6 @@ from tensorflow_metadata.proto.v0 import schema_pb2, anomalies_pb2, statistics_p
 import gcsfs
 
 from absl import logging
-
 import os
 import dotenv
 
@@ -31,171 +30,152 @@ _TRANSFORM_FEATURE_KEYS = [
 ]
 
 def _get_tf_examples_serving_signature(model, tf_transform_output):
-  """Returns a serving signature that accepts `tensorflow.Example`."""
+    """Returns a serving signature that accepts `tensorflow.Example`."""
+    model.tft_layer_inference = tf_transform_output.transform_features_layer()
 
-  # We need to track the layers in the model in order to save it.
-  # TODO: Revise once the bug is resolved.
-  model.tft_layer_inference = tf_transform_output.transform_features_layer()
+    @tf.function(input_signature=[
+        tf.TensorSpec(shape=[None], dtype=tf.string, name='examples')
+    ])
+    def serve_tf_examples_fn(serialized_tf_example):
+        raw_feature_spec = tf_transform_output.raw_feature_spec()
+        raw_feature_spec.pop(_LABEL_KEY)
+        required_feature_spec = {
+            k: v for k, v in raw_feature_spec.items() if k in _FEATURE_KEYS
+        }
+        raw_features = tf.io.parse_example(serialized_tf_example, required_feature_spec)
+        transformed_features = model.tft_layer_inference(raw_features)
+        outputs = model(transformed_features)
+        return {'outputs': outputs}
 
-  @tf.function(input_signature=[
-      tf.TensorSpec(shape=[None], dtype=tf.string, name='examples')
-  ])
-  def serve_tf_examples_fn(serialized_tf_example):
-    
-    """Returns the output to be used in the serving signature."""
-    raw_feature_spec = tf_transform_output.raw_feature_spec()
-
-    # Remove label feature and other features that will not be present at serving time.
-    raw_feature_spec.pop(_LABEL_KEY)
-    required_feature_spec = {
-        k: v for k, v in raw_feature_spec.items() if k in _FEATURE_KEYS
-    }
-
-    raw_features = tf.io.parse_example(serialized_tf_example, required_feature_spec)
-    transformed_features = model.tft_layer_inference(raw_features)
-    logging.info('serve_transformed_features = %s', transformed_features)
-
-    outputs = model(transformed_features)
-    return {'outputs': outputs}
-
-  return serve_tf_examples_fn
+    return serve_tf_examples_fn
 
 def _get_transform_features_signature(model, tf_transform_output):
-  """Returns a serving signature that applies tf.Transform to features."""
+    model.tft_layer_eval = tf_transform_output.transform_features_layer()
 
-  # We need to track the layers in the model in order to save it.
-  # TODO: Revise once the bug is resolved.
-  model.tft_layer_eval = tf_transform_output.transform_features_layer()
+    @tf.function(input_signature=[
+        tf.TensorSpec(shape=[None], dtype=tf.string, name='examples')
+    ])
+    def transform_features_fn(serialized_tf_example):
+        raw_feature_spec = tf_transform_output.raw_feature_spec()
+        raw_features = tf.io.parse_example(serialized_tf_example, raw_feature_spec)
+        transformed_features = model.tft_layer_eval(raw_features)
+        return transformed_features
 
-  @tf.function(input_signature=[
-      tf.TensorSpec(shape=[None], dtype=tf.string, name='examples')
-  ])
-  def transform_features_fn(serialized_tf_example):
-    """Returns the transformed_features to be fed as input to evaluator."""
-    raw_feature_spec = tf_transform_output.raw_feature_spec()
-    print("Raw feature spec:", raw_feature_spec)
-
-    raw_features = tf.io.parse_example(serialized_tf_example, raw_feature_spec)
-    transformed_features = model.tft_layer_eval(raw_features)
-    logging.info('eval_transformed_features = %s', transformed_features)
-    return transformed_features
-
-  return transform_features_fn
+    return transform_features_fn
 
 def input_fn(file_pattern, tf_transform_output, batch_size=200):
-        transformed_feature_spec = (
-            tf_transform_output.transformed_feature_spec().copy()
-        )
-        print("Transformed feature spec:", transformed_feature_spec)
-
-        dataset = tf.data.experimental.make_batched_features_dataset(
-            file_pattern=file_pattern,
-            batch_size=batch_size,
-            features=transformed_feature_spec,
-            reader=lambda filenames: tf.data.TFRecordDataset(filenames, compression_type='GZIP'),
-            label_key=_LABEL_KEY
-        )
-        print("Dataset element spec:", dataset.element_spec)
-
-        return dataset
+    transformed_feature_spec = tf_transform_output.transformed_feature_spec().copy()
+    dataset = tf.data.experimental.make_batched_features_dataset(
+        file_pattern=file_pattern,
+        batch_size=batch_size,
+        features=transformed_feature_spec,
+        reader=lambda filenames: tf.data.TFRecordDataset(filenames, compression_type='GZIP'),
+        label_key=_LABEL_KEY
+    )
+    return dataset
 
 def export_serving_model(tf_transform_output, model, output_dir):
-  """Exports a keras model for serving.
-  Args:
-    tf_transform_output: Wrapper around output of tf.Transform.
-    model: A keras model to export for serving.
-    output_dir: A directory where the model will be exported to.
-  """
-  # The layer has to be saved to the model for keras tracking purposes.
-  model.tft_layer = tf_transform_output.transform_features_layer()
+    model.tft_layer = tf_transform_output.transform_features_layer()
+    signatures = {
+        'serving_default': _get_tf_examples_serving_signature(model, tf_transform_output),
+        'transform_features': _get_transform_features_signature(model, tf_transform_output),
+    }
+    model.save(output_dir, save_format='tf', signatures=signatures)
 
-  signatures = {
-      'serving_default':
-          _get_tf_examples_serving_signature(model, tf_transform_output),
-      'transform_features':
-          _get_transform_features_signature(model, tf_transform_output),
-  }
-
-  model.save(output_dir, save_format='tf', signatures=signatures)
-
-def _build_keras_model(tf_transform_output: TFTransformOutput
-                       ) -> tf.keras.Model:
-    """Creates a CNN Keras model for predicting fare amount.
-
-    Returns:
-        A Keras Model.
-    """
-
+def _build_keras_model(tf_transform_output: TFTransformOutput) -> tf.keras.Model:
     feature_spec = tf_transform_output.transformed_feature_spec().copy()
     feature_spec.pop(_LABEL_KEY)
 
-    inputs = {}
+    numerical_inputs = {}
+    categorical_inputs = {}
+
+    # Define inputs and separate them into numerical and categorical
     for key, spec in feature_spec.items():
-        if isinstance(spec, tf.io.VarLenFeature):
-            inputs[key] = tf.keras.layers.Input(
-                shape=[None], name=key, dtype=spec.dtype, sparse=True)
-        elif isinstance(spec, tf.io.FixedLenFeature):
-            inputs[key] = tf.keras.layers.Input(
-                shape=spec.shape or [1], name=key, dtype=spec.dtype)
+        if isinstance(spec, tf.io.FixedLenFeature):
+            if spec.dtype == tf.float32:
+                numerical_inputs[key] = tf.keras.layers.Input(shape=[1], name=key, dtype=spec.dtype)
+            elif spec.dtype == tf.string:
+                categorical_inputs[key] = tf.keras.layers.Input(shape=[1], name=key, dtype=spec.dtype)
         else:
-            raise ValueError('Spec type is not supported: ', key, spec)
-          
-    x = tf.keras.layers.Concatenate()(tf.nest.flatten(inputs))
-    x = tf.keras.layers.Dense(512, activation='relu')(x)
+            raise ValueError('Unsupported feature type: ', key, spec)
+
+    # Process numerical features
+    concatenated_numerical_features = tf.keras.layers.Concatenate()(list(numerical_inputs.values()))
+
+    # Process categorical features
+    def encode_string_inputs(inputs):
+        encoded_layers = []
+        for input_name, input_tensor in inputs.items():
+            vocab_size = 1000  # Adjust according to the feature
+            embedding_dim = 16
+            num_oov_buckets = 1
+
+            lookup = tf.keras.layers.StringLookup(
+                vocabulary=None,
+                num_oov_indices=num_oov_buckets,
+                mask_token=None,
+                output_mode="int",
+                name=f"{input_name}_lookup"
+            )
+            encoded_layer = lookup(input_tensor)
+            embedding = tf.keras.layers.Embedding(vocab_size + num_oov_buckets, embedding_dim)(encoded_layer)
+            encoded_layer = tf.keras.layers.Reshape(target_shape=(embedding_dim,))(embedding)
+            encoded_layers.append(encoded_layer)
+        return encoded_layers
+
+    # Encode and concatenate categorical features
+    encoded_categorical_features = encode_string_inputs(categorical_inputs)
+    concatenated_categorical_features = tf.keras.layers.Concatenate()(encoded_categorical_features)
+
+    # Combine all features
+    concatenated_features = tf.keras.layers.Concatenate()([concatenated_numerical_features, concatenated_categorical_features])
+
+    # Build the rest of the model
+    x = tf.keras.layers.Dense(512, activation='relu')(concatenated_features)
     x = tf.keras.layers.Dense(256, activation='relu')(x)
     x = tf.keras.layers.Dense(128, activation='relu')(x)
     x = tf.keras.layers.Dense(64, activation='relu')(x)
     x = tf.keras.layers.Dense(32, activation='relu')(x)
     output = tf.keras.layers.Dense(1)(x)
 
-    return tf.keras.Model(inputs=inputs, outputs=output)
+    return tf.keras.Model(inputs={**numerical_inputs, **categorical_inputs}, outputs=output)
 
 def run_fn(fn_args):
-   """Train the model based on given args.
+    tf_transform_output = TFTransformOutput(fn_args.transform_output)
+    train_dataset = input_fn(fn_args.train_files, tf_transform_output)
+    eval_dataset = input_fn(fn_args.eval_files, tf_transform_output)
+    model = _build_keras_model(tf_transform_output)
 
-   Args:
-       fn_args: Holds args used to train the model as name/value pairs.
-   """
-   tf_transform_output = TFTransformOutput(fn_args.transform_output)
-
-   train_dataset = input_fn(
-       fn_args.train_files,
-       tf_transform_output)
-   eval_dataset = input_fn(
-       fn_args.eval_files,
-       tf_transform_output)
-
-   model = _build_keras_model(tf_transform_output)
-
-   early_stopping = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=5)
-
-   lr_schedule = tf.keras.optimizers.schedules.ExponentialDecay(
+    early_stopping = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=5)
+    lr_schedule = tf.keras.optimizers.schedules.ExponentialDecay(
         initial_learning_rate=1e-1,
         decay_steps=1000,
-        decay_rate=0.9)
-
-   model.compile(
-        optimizer=tf.keras.optimizers.Adam(learning_rate=lr_schedule),
-        loss='mean_squared_error',  # Using MSE for regression
-        metrics=['mean_absolute_error']  # MAE as an additional metric
+        decay_rate=0.9
     )
 
-   tensorboard_callback = tf.keras.callbacks.TensorBoard(
-      log_dir=fn_args.model_run_dir, update_freq='batch')
-   print("Training logs saved to: " + fn_args.model_run_dir)
+    model.compile(
+        optimizer=tf.keras.optimizers.Adam(learning_rate=lr_schedule),
+        loss='mean_squared_error',
+        metrics=['mean_absolute_error']
+    )
+
+    tensorboard_callback = tf.keras.callbacks.TensorBoard(
+        log_dir=fn_args.model_run_dir, update_freq='batch'
+    )
    
-   model.fit(
-      train_dataset,
-      steps_per_epoch=fn_args.train_steps,
-      validation_data=eval_dataset,
-      validation_steps=fn_args.eval_steps,
-      callbacks=[tensorboard_callback, early_stopping])
-   # Ensure the transformation layer is saved with the model
-   export_serving_model(tf_transform_output, model, fn_args.serving_model_dir)
+    model.fit(
+        train_dataset,
+        steps_per_epoch=fn_args.train_steps,
+        validation_data=eval_dataset,
+        validation_steps=fn_args.eval_steps,
+        callbacks=[tensorboard_callback, early_stopping]
+    )
+   
+    export_serving_model(tf_transform_output, model, fn_args.serving_model_dir)
 
 def create_trainer(transform, schema_gen, module_file):
     return Trainer(
-        module_file=module_file, 
+        module_file=module_file,
         custom_config={
             'ai_platform_training_args': {
                 'project': GOOGLE_CLOUD_PROJECT,
