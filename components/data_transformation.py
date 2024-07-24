7c739bd4abd1a9da32cb201e32c55e47e2a4f5da
Erbli Kuka <42184398+ekuka86@users.noreply.github.com>
2024-07-24 23:42:45 +0200

    minor changes

--- components/data_transformation.py
+++ components/data_transformation.py
@@ -4,75 +4,54 @@ import tensorflow_transform as tft
 from tfx.proto import transform_pb2
 
 # Define feature keys and label key
-_NUMERICAL_FEATURES = ["TripMiles", "TripSeconds"]
-_CATEGORICAL_NUMERICAL_FEATURES = ["PickupCommunityArea", "DropoffCommunityArea"]
-_CATEGORICAL_STRING_FEATURES = ["TripStartTimestamp", "TripEndTimestamp", "PaymentType", "Company"]
-_BUCKET_FEATURES = ["PickupCommunityArea", "DropoffCommunityArea"]
-_LABEL_KEY = 'Fare'
-_FEATURE_BUCKET_COUNT = 10
-_VOCAB_SIZE = 1000
-_OOV_SIZE = 1
+_FEATURE_KEYS = [
+    "TripSeconds", "TripMiles", "PickupCommunityArea", "DropoffCommunityArea", 
+    "TripStartTimestamp", "TripEndTimestamp", "PaymentType", "Company"
+]
 
-# Utility function for renaming keys
+NUMERICAL_FEATURES = ['TripSeconds', 'TripMiles']
+
+CATEGORICAL_NUMERICAL_FEATURES = [
+    'PickupCommunityArea', 'DropoffCommunityArea'
+]
+
+CATEGORICAL_STRING_FEATURES = [
+    'PaymentType', 'Company'
+]
+
+VOCAB_SIZE = 1000
+OOV_SIZE = 10
+LABEL_KEY = 'Fare'
+
 def t_name(key):
     return key + '_xf'
 
 def _make_one_hot(x, key):
-    """Make a one-hot tensor to encode categorical features.
-    Args:
-        X: A dense tensor
-        key: A string key for the feature in the input
-    Returns:
-        A dense one-hot tensor as a float list
-    """
-    integerized = tft.compute_and_apply_vocabulary(
-        x,
-        top_k=_VOCAB_SIZE,
-        num_oov_buckets=_OOV_SIZE,
-        vocab_filename=key, name=key)
-    depth = tft.experimental.get_vocabulary_size_by_name(key) + _OOV_SIZE
+    integerized = tft.compute_and_apply_vocabulary(x,
+            top_k=VOCAB_SIZE,
+            num_oov_buckets=OOV_SIZE,
+            vocab_filename=key, name=key)
+    depth = (
+        tft.experimental.get_vocabulary_size_by_name(key) + OOV_SIZE)
     one_hot_encoded = tf.one_hot(
-        integerized, depth=tf.cast(depth, tf.int32),
-        on_value=1.0, off_value=0.0)
+        integerized,
+        depth=tf.cast(depth, tf.int32),
+        on_value=1.0,
+        off_value=0.0)
     return tf.reshape(one_hot_encoded, [-1, depth])
 
 def preprocessing_fn(inputs):
-    """tf.transform's callback function for preprocessing inputs.
-    Args:
-        inputs: map from feature keys to raw not-yet-transformed features.
-    Returns:
-        Map from string feature key to transformed feature operations.
-    """
     outputs = {}
-    
-    # Scale numerical features to z-score
-    for key in _NUMERICAL_FEATURES:
+    for key in NUMERICAL_FEATURES:
         outputs[t_name(key)] = tft.scale_to_z_score(inputs[key], name=key)
 
-    # Bucketize numerical features to categorical
-    for key in _BUCKET_FEATURES:
-        outputs[t_name(key)] = tf.cast(tft.bucketize(inputs[key], _FEATURE_BUCKET_COUNT, name=key), dtype=tf.float32)
-
-    # Apply one-hot encoding to string categorical features
-    for key in _CATEGORICAL_STRING_FEATURES:
+    for key in CATEGORICAL_STRING_FEATURES:
         outputs[t_name(key)] = _make_one_hot(inputs[key], key)
 
-    # Apply one-hot encoding to numerical categorical features after converting them to string
-    for key in _CATEGORICAL_NUMERICAL_FEATURES:
-        outputs[t_name(key)] = _make_one_hot(tf.strings.strip(tf.strings.as_string(inputs[key])), key)
-
-    # Pass through the label key
-    outputs[_LABEL_KEY] = inputs[_LABEL_KEY]
-
+    for key in CATEGORICAL_NUMERICAL_FEATURES:
+        outputs[t_name(key)] = _make_one_hot(tf.strings.strip(
+            tf.strings.as_string(inputs[key])), key)
+    
+    # Output the label
+    outputs[LABEL_KEY] = inputs[LABEL_KEY]
     return outputs
-
-def create_transform(example_gen, schema_gen):
-    return Transform(
-        examples=example_gen.outputs['examples'],
-        schema=schema_gen.outputs['schema'],
-        module_file="components/data_transformation.py",
-        splits_config=transform_pb2.SplitsConfig(
-            analyze=['train'],
-            transform=['train', 'eval']
-        )
-    )
--- components/model_trainer.py
+++ components/model_trainer.py
@@ -1,15 +1,13 @@
-from tfx.components import Trainer
-from tfx.proto import trainer_pb2
-from tfx import v1 as tfx
+from typing import List, Text
+import os
 import tensorflow as tf
 import tensorflow_transform as tft
-from tensorflow_transform import TFTransformOutput
+from tfx import v1 as tfx
+from tfx.components import Trainer
+from tfx.proto import trainer_pb2
 from tfx_bsl.public import tfxio
-from tensorflow_metadata.proto.v0 import schema_pb2, anomalies_pb2, statistics_pb2
-import gcsfs
-
-from absl import logging
-import os
+from tensorflow_transform import TFTransformOutput
+from tensorflow_metadata.proto.v0 import schema_pb2, statistics_pb2, anomalies_pb2
 import dotenv
 
 dotenv.load_dotenv()
@@ -19,18 +17,19 @@ GOOGLE_CLOUD_REGION = os.getenv("GOOGLE_CLOUD_REGION")
 GCS_BUCKET_NAME = os.getenv("GCS_BUCKET_NAME")
 
 _LABEL_KEY = 'Fare'
-_FEATURE_KEYS = [
-    "TripSeconds", "TripMiles", "PickupCommunityArea", "DropoffCommunityArea",
-    "TripStartTimestamp", "TripEndTimestamp", "PaymentType", "Company"
-]
-_TRANSFORM_FEATURE_KEYS = [
-    "TripSeconds_xf", "TripMiles_xf", "PickupCommunityArea_xf", 
-    "DropoffCommunityArea_xf", "TripStartTimestamp_xf", "TripEndTimestamp_xf", 
-    "PaymentType_xf", "Company_xf"
-]
+_BATCH_SIZE = 40
+
+def _input_fn(file_pattern: List[Text],
+              data_accessor: tfx.components.DataAccessor,
+              tf_transform_output: tft.TFTransformOutput,
+              batch_size: int = 200) -> tf.data.Dataset:
+    return data_accessor.tf_dataset_factory(
+        file_pattern,
+        tfxio.TensorFlowDatasetOptions(
+            batch_size=batch_size, label_key=_LABEL_KEY),
+        tf_transform_output.transformed_metadata.schema)
 
 def _get_tf_examples_serving_signature(model, tf_transform_output):
-    """Returns a serving signature that accepts `tensorflow.Example`."""
     model.tft_layer_inference = tf_transform_output.transform_features_layer()
 
     @tf.function(input_signature=[
@@ -39,14 +38,11 @@ def _get_tf_examples_serving_signature(model, tf_transform_output):
     def serve_tf_examples_fn(serialized_tf_example):
         raw_feature_spec = tf_transform_output.raw_feature_spec()
         raw_feature_spec.pop(_LABEL_KEY)
-        required_feature_spec = {
-            k: v for k, v in raw_feature_spec.items() if k in _FEATURE_KEYS
-        }
-        raw_features = tf.io.parse_example(serialized_tf_example, required_feature_spec)
+        raw_features = tf.io.parse_example(serialized_tf_example, raw_feature_spec)
         transformed_features = model.tft_layer_inference(raw_features)
         outputs = model(transformed_features)
         return {'outputs': outputs}
-
+    
     return serve_tf_examples_fn
 
 def _get_transform_features_signature(model, tf_transform_output):
@@ -60,19 +56,8 @@ def _get_transform_features_signature(model, tf_transform_output):
         raw_features = tf.io.parse_example(serialized_tf_example, raw_feature_spec)
         transformed_features = model.tft_layer_eval(raw_features)
         return transformed_features
-
+    
     return transform_features_fn
-
-def input_fn(file_pattern, tf_transform_output, batch_size=200):
-    transformed_feature_spec = tf_transform_output.transformed_feature_spec().copy()
-    dataset = tf.data.experimental.make_batched_features_dataset(
-        file_pattern=file_pattern,
-        batch_size=batch_size,
-        features=transformed_feature_spec,
-        reader=lambda filenames: tf.data.TFRecordDataset(filenames, compression_type='GZIP'),
-        label_key=_LABEL_KEY
-    )
-    return dataset
 
 def export_serving_model(tf_transform_output, model, output_dir):
     model.tft_layer = tf_transform_output.transform_features_layer()
@@ -86,96 +71,55 @@ def _build_keras_model(tf_transform_output: TFTransformOutput) -> tf.keras.Model
     feature_spec = tf_transform_output.transformed_feature_spec().copy()
     feature_spec.pop(_LABEL_KEY)
 
-    numerical_inputs = {}
-    categorical_inputs = {}
-
-    # Define inputs and separate them into numerical and categorical
+    inputs = {}
     for key, spec in feature_spec.items():
-        if isinstance(spec, tf.io.FixedLenFeature):
-            if spec.dtype == tf.float32:
-                numerical_inputs[key] = tf.keras.layers.Input(shape=[1], name=key, dtype=spec.dtype)
-            elif spec.dtype == tf.string:
-                categorical_inputs[key] = tf.keras.layers.Input(shape=[1], name=key, dtype=spec.dtype)
+        if isinstance(spec, tf.io.VarLenFeature):
+            inputs[key] = tf.keras.layers.Input(
+                shape=[None], name=key, dtype=spec.dtype, sparse=True)
+        elif isinstance(spec, tf.io.FixedLenFeature):
+            inputs[key] = tf.keras.layers.Input(
+                shape=spec.shape or [1], name=key, dtype=spec.dtype)
         else:
-            raise ValueError('Unsupported feature type: ', key, spec)
+            raise ValueError('Spec type is not supported: ', key, spec)
 
-    # Process numerical features
-    concatenated_numerical_features = tf.keras.layers.Concatenate()(list(numerical_inputs.values()))
+    output = tf.keras.layers.Concatenate()(tf.nest.flatten(inputs))
+    output = tf.keras.layers.Dense(100, activation='relu')(output)
+    output = tf.keras.layers.Dense(70, activation='relu')(output)
+    output = tf.keras.layers.Dense(50, activation='relu')(output)
+    output = tf.keras.layers.Dense(20, activation='relu')(output)
+    output = tf.keras.layers.Dense(1)(output)
+    return tf.keras.Model(inputs=inputs, outputs=output)
 
-    # Process categorical features
-    def encode_string_inputs(inputs):
-        encoded_layers = []
-        for input_name, input_tensor in inputs.items():
-            vocab_size = 1000  # Adjust according to the feature
-            embedding_dim = 16
-            num_oov_buckets = 1
-
-            lookup = tf.keras.layers.StringLookup(
-                vocabulary=None,
-                num_oov_indices=num_oov_buckets,
-                mask_token=None,
-                output_mode="int",
-                name=f"{input_name}_lookup"
-            )
-            encoded_layer = lookup(input_tensor)
-            embedding = tf.keras.layers.Embedding(vocab_size + num_oov_buckets, embedding_dim)(encoded_layer)
-            encoded_layer = tf.keras.layers.Reshape(target_shape=(embedding_dim,))(embedding)
-            encoded_layers.append(encoded_layer)
-        return encoded_layers
-
-    # Encode and concatenate categorical features
-    encoded_categorical_features = encode_string_inputs(categorical_inputs)
-    concatenated_categorical_features = tf.keras.layers.Concatenate()(encoded_categorical_features)
-
-    # Combine all features
-    concatenated_features = tf.keras.layers.Concatenate()([concatenated_numerical_features, concatenated_categorical_features])
-
-    # Build the rest of the model
-    x = tf.keras.layers.Dense(512, activation='relu')(concatenated_features)
-    x = tf.keras.layers.Dense(256, activation='relu')(x)
-    x = tf.keras.layers.Dense(128, activation='relu')(x)
-    x = tf.keras.layers.Dense(64, activation='relu')(x)
-    x = tf.keras.layers.Dense(32, activation='relu')(x)
-    output = tf.keras.layers.Dense(1)(x)
-
-    return tf.keras.Model(inputs={**numerical_inputs, **categorical_inputs}, outputs=output)
-
-def run_fn(fn_args):
-    tf_transform_output = TFTransformOutput(fn_args.transform_output)
-    train_dataset = input_fn(fn_args.train_files, tf_transform_output)
-    eval_dataset = input_fn(fn_args.eval_files, tf_transform_output)
+def run_fn(fn_args: tfx.components.FnArgs):
+    tf_transform_output = tft.TFTransformOutput(fn_args.transform_output)
+    train_dataset = _input_fn(fn_args.train_files, fn_args.data_accessor,
+                              tf_transform_output, _LABEL_KEY)
+    eval_dataset = _input_fn(fn_args.eval_files, fn_args.data_accessor,
+                             tf_transform_output, _LABEL_KEY)
+    
     model = _build_keras_model(tf_transform_output)
-
-    early_stopping = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=5)
-    lr_schedule = tf.keras.optimizers.schedules.ExponentialDecay(
-        initial_learning_rate=1e-1,
-        decay_steps=1000,
-        decay_rate=0.9
-    )
-
+    
     model.compile(
-        optimizer=tf.keras.optimizers.Adam(learning_rate=lr_schedule),
-        loss='mean_squared_error',
-        metrics=['mean_absolute_error']
-    )
+        loss=tf.keras.losses.MeanSquaredError(),
+        optimizer=tf.keras.optimizers.Adam(learning_rate=0.001),
+        metrics=[tf.keras.metrics.MeanAbsoluteError()])
 
     tensorboard_callback = tf.keras.callbacks.TensorBoard(
-        log_dir=fn_args.model_run_dir, update_freq='batch'
-    )
-   
+        log_dir=fn_args.model_run_dir, update_freq='batch')
+
     model.fit(
         train_dataset,
         steps_per_epoch=fn_args.train_steps,
         validation_data=eval_dataset,
         validation_steps=fn_args.eval_steps,
-        callbacks=[tensorboard_callback, early_stopping]
-    )
-   
+        callbacks=[tensorboard_callback])
+
     export_serving_model(tf_transform_output, model, fn_args.serving_model_dir)
 
-def create_trainer(transform, schema_gen, module_file):
+def create_trainer(transform, schema_gen):
     return Trainer(
-        module_file=module_file,
+        module_file=os.path.abspath(__file__),
+        custom_executor_spec=tfx.dsl.components.base.executor_spec.ExecutorClassSpec(run_fn),
         custom_config={
             'ai_platform_training_args': {
                 'project': GOOGLE_CLOUD_PROJECT,
