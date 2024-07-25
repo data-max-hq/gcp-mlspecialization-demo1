5b767b1c8c9c98c2c7b025f66452f8da6dd6dd94
Erbli Kuka <42184398+ekuka86@users.noreply.github.com>
2024-07-25 17:03:16 +0200

    test

--- pipeline/pipeline_definition.py
+++ pipeline/pipeline_definition.py
@@ -1,39 +1,61 @@
-import tfx
 from tfx.orchestration import pipeline
-from tfx.extensions.google_cloud_ai_platform.trainer import executor as ai_platform_trainer_executor
-from tfx.extensions.google_cloud_ai_platform.pusher import executor as ai_platform_pusher_executor
+from tfx.orchestration.kubeflow.v2 import KubeflowV2DagRunner
+
 from components.data_ingestion import create_example_gen
 from components.data_validation import create_data_validation
 from components.data_transformation import create_transform
 from components.model_trainer import create_trainer
 from components.model_evaluator_and_pusher import create_evaluator_and_pusher
 
-def create_pipeline(pipeline_name: str, pipeline_root: str, data_path: str, serving_model_dir: str, module_file: str, project: str, region: str):
+def create_pipeline(pipeline_name: str, pipeline_root: str, data_path: str, serving_model_dir:str,module_file:str,project:str,region:str):
     example_gen = create_example_gen(data_path)
     statistics_gen, schema_gen, example_validator = create_data_validation(example_gen)
 
     transform = create_transform(example_gen, schema_gen)
     
     trainer = create_trainer(transform, schema_gen, module_file)
-
-    # Custom configurations dictionary for each component for GCP
-    ai_platform_training_args = {
-        'region': region,
-        'project': project,
-        'jobDir': f'gs://dataset_bucket_demo1/{pipeline_name}',
-        'scaleTier': 'CUSTOM',
-        'masterType':  'n1-highmem-16'
-    }
     
     evaluator, pusher, resolver = create_evaluator_and_pusher(example_gen, trainer, serving_model_dir)
-    
-    # Use the GCP-specific trainer and pusher executors
-    trainer.ExecutorClassSpec = tfx.dsl.components.executor_spec.ExecutorClassSpec(ai_platform_trainer_executor.GenericExecutor)
-    pusher.ExecutorClassSpec = tfx.dsl.components.executor_spec.ExecutorClassSpec(ai_platform_pusher_executor.Executor)
-    
-    # Pass the custom configurations to the components
-    transform.custom_config = ai_platform_training_args
-    trainer.custom_config = ai_platform_training_args
+
+    # Define KubeflowV2DagRunner with machine type settings
+    runner = KubeflowV2DagRunner(
+        config={
+            'pipeline_runtime_spec': {
+                'executor_specs': {
+                    'Transform_executor': {
+                        'container': {
+                            'image': 'gcr.io/tfx-oss-public/tfx:1.15.1',
+                            'resources': {
+                                'requests': {
+                                    'memory': '8Gi'  # Set memory request for Transform
+                                }
+                            }
+                        }
+                    },
+                    'Trainer_executor': {
+                        'container': {
+                            'image': 'gcr.io/tfx-oss-public/tfx:1.15.1',
+                            'resources': {
+                                'requests': {
+                                    'memory': '16Gi'  # Set memory request for Trainer
+                                }
+                            }
+                        }
+                    },
+                    'Evaluator_executor': {
+                        'container': {
+                            'image': 'gcr.io/tfx-oss-public/tfx:1.15.1',
+                            'resources': {
+                                'requests': {
+                                    'memory': '8Gi'  # Set memory request for Evaluator
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    )
 
     return pipeline.Pipeline(
         pipeline_name=pipeline_name,
@@ -48,5 +70,6 @@ def create_pipeline(pipeline_name: str, pipeline_root: str, data_path: str, serv
             resolver,
             evaluator,
             pusher
-        ]
+        ],
+        enable_cache=True,
     )
