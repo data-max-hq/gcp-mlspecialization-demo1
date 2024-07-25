d22294512a3815cc0bd62b211d53a8b70c83d2d5
Erbli Kuka <42184398+ekuka86@users.noreply.github.com>
2024-07-25 15:35:45 +0200

    minor changes

--- pipeline/pipeline_definition.py
+++ pipeline/pipeline_definition.py
@@ -1,19 +1,22 @@
 from tfx.orchestration import pipeline
-from tfx.orchestration import metadata
-
 
 from components.data_ingestion import create_example_gen
 from components.data_validation import create_data_validation
 from components.data_transformation import create_transform
 from components.model_trainer import create_trainer
 from components.model_evaluator_and_pusher import create_evaluator_and_pusher
+from kfp.v2.dsl import pipeline, component
 
+
+@component(
+    cpu_limit='8',  # Example specifying CPU limit
+    memory_limit='64G'  # Specifying memory limit
+)
 
 def create_pipeline(pipeline_name: str, pipeline_root: str, data_path: str, serving_model_dir:str,module_file:str,project:str,region:str):
     example_gen = create_example_gen(data_path)
     statistics_gen, schema_gen, example_validator = create_data_validation(example_gen)
     transform = create_transform(example_gen, schema_gen)
-    transform.set_memory_limit('64G')  # Set 64GB RAM
     trainer = create_trainer(transform, schema_gen, module_file)
     evaluator, pusher, resolver = create_evaluator_and_pusher(example_gen, trainer, serving_model_dir)
 
