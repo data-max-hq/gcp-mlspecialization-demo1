3161884d1ad0d2eeecca5ee17cc2f5e48a289047
Erbli Kuka <42184398+ekuka86@users.noreply.github.com>
2024-07-28 22:35:01 +0200

    changes

--- components/model_evaluator_and_pusher.py
+++ components/model_evaluator_and_pusher.py
@@ -1,7 +1,7 @@
-# components/model_evaluator_and_pusher.py
-
-from tfx.components import Pusher
+from tfx.components import Evaluator, Pusher
+from tfx.proto import pusher_pb2, evaluator_pb2
 from tfx import v1 as tfx
+import tensorflow_model_analysis as tfma
 import os
 import dotenv
 
@@ -11,78 +11,72 @@ project_id = os.getenv("GOOGLE_CLOUD_PROJECT")
 endpoint_name = os.getenv("SERVING_ENDPOINT_NAME")
 region = os.getenv("GOOGLE_CLOUD_REGION")
 
+eval_config = tfma.EvalConfig(
+    model_specs=[
+        # This assumes a serving model with signature 'serving_default'. If
+        # using estimator based EvalSavedModel, add signature_name: 'eval' and
+        # remove the label_key.
+        tfma.ModelSpec(
+            signature_name='serving_default',
+            label_key='Purchase',
+            preprocessing_function_names=['transform_features'],
+            )
+        ],
+    slicing_specs=[
+        tfma.SlicingSpec()
+    ],
+    metrics_specs=[
+        tfma.MetricsSpec(
+            metrics=[
+                tfma.MetricConfig(class_name='RootMeanSquaredError', threshold=tfma.MetricThreshold(value_threshold=tfma.GenericValueThreshold(upper_bound={'value': 10000000})))
+                ])]
+    )
+
+
 vertex_serving_spec = {
-    'project_id': project_id,
-    'endpoint_name': endpoint_name,
-    'machine_type': 'n1-standard-2',
-}
+      'project_id': project_id,
+      'endpoint_name': endpoint_name,
+      # Remaining argument is passed to aiplatform.Model.deploy()
+      # See https://cloud.google.com/vertex-ai/docs/predictions/deploy-model-api#deploy_the_model
+      # for the detail.
+      #
+      # Machine type is the compute resource to serve prediction requests.
+      # See https://cloud.google.com/vertex-ai/docs/predictions/configure-compute#machine-types
+      # for available machine types and acccerators.
+      'machine_type': 'n1-standard-2',
+  }
 
 serving_image = "us-docker.pkg.dev/vertex-ai-restricted/prediction/tf_opt-cpu.2-13:latest"
 
+
 def create_evaluator_and_pusher(example_gen, trainer, serving_model_dir):
-    # Check if there's a previous model
-    resolver = tfx.dsl.Resolver(
-        strategy_class=tfx.dsl.experimental.LatestBlessedModelStrategy,
-        model=tfx.dsl.Channel(type=tfx.types.standard_artifacts.Model),
-        model_blessing=tfx.dsl.Channel(
-            type=tfx.types.standard_artifacts.ModelBlessing)).with_id(
-                'latest_blessed_model_resolver')
+    
+    model_resolver = tfx.dsl.Resolver(
+    strategy_class=tfx.dsl.experimental.LatestBlessedModelStrategy,
+    model=tfx.dsl.Channel(type=tfx.types.standard_artifacts.Model),
+    model_blessing=tfx.dsl.Channel(
+        type=tfx.types.standard_artifacts.ModelBlessing)).with_id(
+            'latest_blessed_model_resolver')
 
-    evaluator_needed = False
-    try:
-        resolver.outputs['model'].get()[0]
-        evaluator_needed = True
-    except IndexError:
-        print("No previous model found. Skipping evaluation.")
-
-    if evaluator_needed:
-        eval_config = tfma.EvalConfig(
-            model_specs=[
-                tfma.ModelSpec(
-                    signature_name='serving_default',
-                    label_key='Purchase',
-                    preprocessing_function_names=['transform_features'],
-                )
-            ],
-            slicing_specs=[
-                tfma.SlicingSpec()
-            ],
-            metrics_specs=[
-                tfma.MetricsSpec(
-                    metrics=[
-                        tfma.MetricConfig(class_name='RootMeanSquaredError', threshold=tfma.MetricThreshold(value_threshold=tfma.GenericValueThreshold(upper_bound={'value': 10000000})))
-                    ]
-                )
-            ]
-        )
-
-        evaluator = Evaluator(
-            examples=example_gen.outputs['examples'],
-            model=trainer.outputs['model'],
-            baseline_model=resolver.outputs['model'],
-            eval_config=eval_config,
-            example_splits=['test']
-        )
-
-        pusher = tfx.extensions.google_cloud_ai_platform.Pusher(
-            model=trainer.outputs['model'],
-            model_blessing=evaluator.outputs['blessing'],
-            custom_config={
-                tfx.extensions.google_cloud_ai_platform.ENABLE_VERTEX_KEY: True,
-                tfx.extensions.google_cloud_ai_platform.VERTEX_REGION_KEY: region,
-                tfx.extensions.google_cloud_ai_platform.VERTEX_CONTAINER_IMAGE_URI_KEY: serving_image,
-                tfx.extensions.google_cloud_ai_platform.SERVING_ARGS_KEY: vertex_serving_spec,
-            }
-        )
-        return evaluator, pusher, resolver
+    evaluator = Evaluator(
+        examples=example_gen.outputs['examples'],
+        model=trainer.outputs['model'],
+        baseline_model=model_resolver.outputs['model'],
+        eval_config=eval_config,
+        example_splits=['test']
+    )
 
     pusher = tfx.extensions.google_cloud_ai_platform.Pusher(
-        model=trainer.outputs['model'],
-        custom_config={
-            tfx.extensions.google_cloud_ai_platform.ENABLE_VERTEX_KEY: True,
-            tfx.extensions.google_cloud_ai_platform.VERTEX_REGION_KEY: region,
-            tfx.extensions.google_cloud_ai_platform.VERTEX_CONTAINER_IMAGE_URI_KEY: serving_image,
-            tfx.extensions.google_cloud_ai_platform.SERVING_ARGS_KEY: vertex_serving_spec,
-        }
-    )
-    return None, pusher, None
+      model=trainer.outputs['model'],
+      model_blessing=evaluator.outputs['blessing'],
+      custom_config={
+          tfx.extensions.google_cloud_ai_platform.ENABLE_VERTEX_KEY:
+              True,
+          tfx.extensions.google_cloud_ai_platform.VERTEX_REGION_KEY:
+              region,
+          tfx.extensions.google_cloud_ai_platform.VERTEX_CONTAINER_IMAGE_URI_KEY:
+              serving_image,
+          tfx.extensions.google_cloud_ai_platform.SERVING_ARGS_KEY:
+            vertex_serving_spec,
+      })
+    return evaluator, pusher, model_resolver
--- pipeline/pipeline_definition.py
+++ pipeline/pipeline_definition.py
@@ -1,48 +1,42 @@
-# pipeline/pipeline_definition.py
-
 from tfx.orchestration import pipeline
+
 from components.data_ingestion import create_example_gen
 from components.data_validation import create_data_validation
 from components.data_transformation import create_transform
 from components.model_trainer import create_trainer
 from components.model_evaluator_and_pusher import create_evaluator_and_pusher
+import os
+import dotenv
+dotenv.load_dotenv()
 
-def create_pipeline(pipeline_name: str, pipeline_root: str, data_path: str, serving_model_dir: str, module_file: str, project: str, region: str):
+
+def create_pipeline(pipeline_name: str, pipeline_root: str, data_path: str, serving_model_dir:str,module_file:str,project:str,region:str):
     example_gen = create_example_gen(data_path)
     statistics_gen, schema_gen, example_validator = create_data_validation(example_gen)
+
     transform = create_transform(example_gen, schema_gen)
     trainer = create_trainer(transform, schema_gen, module_file)
-
-    # Use the modified function, which checks for previous models
     evaluator, pusher, resolver = create_evaluator_and_pusher(example_gen, trainer, serving_model_dir)
-
-    # Pipeline components list handle None values
-    components = [
-        example_gen,
-        statistics_gen,
-        schema_gen,
-        example_validator,
-        transform,
-        trainer,
-        pusher
-    ]
-
-    # Optional components
-    if resolver is not None:
-        components.append(resolver)
-    if evaluator is not None:
-        components.append(evaluator)
-
     pipeline_args = [
-        '--project=' + project,
-        '--runner=DataflowRunner',
-        '--temp_location=gs://dataset_bucket_demo1/temp',
-        '--region=' + region
-    ]
+    '--project=indigo-idea-428211-h3',
+    '--runner=DataflowRunner',
+    '--temp_location=gs://dataset_bucket_demo1/temp',
+    '--region=us-central1'
+]
 
     return pipeline.Pipeline(
         pipeline_name=pipeline_name,
         pipeline_root=pipeline_root,
         beam_pipeline_args=pipeline_args,
-        components=components
+        components=[
+            example_gen,
+            statistics_gen,
+            schema_gen,
+            example_validator,
+            transform,
+            trainer,
+            resolver,
+            evaluator,
+            pusher
+        ]
     )
