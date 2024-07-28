58882fa9c5ceab2e414c07a72d94f0b1f3d783be
Erbli Kuka <42184398+ekuka86@users.noreply.github.com>
2024-07-28 21:19:28 +0200

    minor changes

--- components/model_evaluator_and_pusher.py
+++ components/model_evaluator_and_pusher.py
@@ -1,3 +1,5 @@
+# components/model_evaluator_and_pusher.py
+
 from tfx.components import Pusher
 from tfx import v1 as tfx
 import os
@@ -12,20 +14,68 @@ region = os.getenv("GOOGLE_CLOUD_REGION")
 vertex_serving_spec = {
     'project_id': project_id,
     'endpoint_name': endpoint_name,
-    # Remaining argument is passed to aiplatform.Model.deploy()
-    # See https://cloud.google.com/vertex-ai/docs/predictions/deploy-model-api#deploy_the_model
-    # for the details.
-    #
-    # Machine type is the compute resource to serve prediction requests.
-    # See https://cloud.google.com/vertex-ai/docs/predictions/configure-compute#machine-types
-    # for available machine types and accelerators.
     'machine_type': 'n1-standard-2',
 }
 
 serving_image = "us-docker.pkg.dev/vertex-ai-restricted/prediction/tf_opt-cpu.2-13:latest"
 
-# Function modified to only include Pusher without evaluation
-def create_pusher_for_new_model(trainer, serving_model_dir):
+def create_evaluator_and_pusher(example_gen, trainer, serving_model_dir):
+    # Check if there's a previous model
+    resolver = tfx.dsl.Resolver(
+        strategy_class=tfx.dsl.experimental.LatestBlessedModelStrategy,
+        model=tfx.dsl.Channel(type=tfx.types.standard_artifacts.Model),
+        model_blessing=tfx.dsl.Channel(
+            type=tfx.types.standard_artifacts.ModelBlessing)).with_id(
+                'latest_blessed_model_resolver')
+
+    evaluator_needed = False
+    try:
+        resolver.outputs['model'].get()[0]
+        evaluator_needed = True
+    except IndexError:
+        print("No previous model found. Skipping evaluation.")
+
+    if evaluator_needed:
+        eval_config = tfma.EvalConfig(
+            model_specs=[
+                tfma.ModelSpec(
+                    signature_name='serving_default',
+                    label_key='Purchase',
+                    preprocessing_function_names=['transform_features'],
+                )
+            ],
+            slicing_specs=[
+                tfma.SlicingSpec()
+            ],
+            metrics_specs=[
+                tfma.MetricsSpec(
+                    metrics=[
+                        tfma.MetricConfig(class_name='RootMeanSquaredError', threshold=tfma.MetricThreshold(value_threshold=tfma.GenericValueThreshold(upper_bound={'value': 10000000})))
+                    ]
+                )
+            ]
+        )
+
+        evaluator = Evaluator(
+            examples=example_gen.outputs['examples'],
+            model=trainer.outputs['model'],
+            baseline_model=resolver.outputs['model'],
+            eval_config=eval_config,
+            example_splits=['test']
+        )
+
+        pusher = tfx.extensions.google_cloud_ai_platform.Pusher(
+            model=trainer.outputs['model'],
+            model_blessing=evaluator.outputs['blessing'],
+            custom_config={
+                tfx.extensions.google_cloud_ai_platform.ENABLE_VERTEX_KEY: True,
+                tfx.extensions.google_cloud_ai_platform.VERTEX_REGION_KEY: region,
+                tfx.extensions.google_cloud_ai_platform.VERTEX_CONTAINER_IMAGE_URI_KEY: serving_image,
+                tfx.extensions.google_cloud_ai_platform.SERVING_ARGS_KEY: vertex_serving_spec,
+            }
+        )
+        return evaluator, pusher, resolver
+
     pusher = tfx.extensions.google_cloud_ai_platform.Pusher(
         model=trainer.outputs['model'],
         custom_config={
@@ -35,4 +85,4 @@ def create_pusher_for_new_model(trainer, serving_model_dir):
             tfx.extensions.google_cloud_ai_platform.SERVING_ARGS_KEY: vertex_serving_spec,
         }
     )
-    return pusher
+    return None, pusher, None
