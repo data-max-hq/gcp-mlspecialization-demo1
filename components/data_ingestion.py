3e2c0dec59f8298f3e1c1b1c9bcd6c651ee3fe8a
Erbli Kuka <42184398+ekuka86@users.noreply.github.com>
2024-07-29 16:36:02 +0200

    minor changes

--- components/data_ingestion.py
+++ components/data_ingestion.py
@@ -1,7 +1,4 @@
 from tfx.v1.extensions.google_cloud_big_query import BigQueryExampleGen
-from tfx.orchestration import pipeline
-from tfx.orchestration.experimental import KubeflowV2DagRunner
-from tfx.orchestration.experimental import KubeflowV2DagRunnerConfig
 
 def create_example_gen(query: str):
     return BigQueryExampleGen(query=query)
