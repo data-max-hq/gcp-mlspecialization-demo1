e91806ddda0bee298ff29fa8b69f8d63f1589a2b
Erbli Kuka <42184398+ekuka86@users.noreply.github.com>
2024-07-29 15:54:38 +0200

    test

--- components/data_ingestion.py
+++ components/data_ingestion.py
@@ -1,3 +1,6 @@
+from tfx.proto import example_gen_pb2
+from tfx.v1.proto import Output
+from tfx.v1.proto import SplitConfig 
 from tfx.components import BigQueryExampleGen
 
 def create_example_gen(query: str):
