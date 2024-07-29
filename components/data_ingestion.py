67c220b3923512e9c770f4be6648f4bcb98294bc
Erbli Kuka <42184398+ekuka86@users.noreply.github.com>
2024-07-29 13:31:28 +0200

    test

--- components/data_ingestion.py
+++ components/data_ingestion.py
@@ -1,15 +1,17 @@
-from tfx.components import BigQueryExampleGen
+from tfx.extensions.google_cloud_big_query.example_gen.component import BigQueryExampleGen
 from tfx.proto import example_gen_pb2
 from tfx.v1.proto import Output
 from tfx.v1.proto import SplitConfig 
 
 def create_example_gen(query: str):
     output_config = example_gen_pb2.Output(
-        split_config=example_gen_pb2.SplitConfig(splits=[
-            example_gen_pb2.SplitConfig.Split(name='train', hash_buckets=8),
-            example_gen_pb2.SplitConfig.Split(name='eval', hash_buckets=1),
-            example_gen_pb2.SplitConfig.Split(name='test', hash_buckets=1)
-        ])
+        split_config=example_gen_pb2.SplitConfig(
+            splits=[
+                example_gen_pb2.SplitConfig.Split(name='train', hash_buckets=8),
+                example_gen_pb2.SplitConfig.Split(name='eval', hash_buckets=1),
+                example_gen_pb2.SplitConfig.Split(name='test', hash_buckets=1)
+            ]
+        )
     )
     
     example_gen = BigQueryExampleGen(query=query, output_config=output_config)
