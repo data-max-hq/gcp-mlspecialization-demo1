deb0cf5f9fbb194bda4a9607e54e0b3a678faa88
Erbli Kuka <42184398+ekuka86@users.noreply.github.com>
2024-07-29 13:54:46 +0200

    minor changes

--- pipeline/pipeline_definition.py
+++ pipeline/pipeline_definition.py
@@ -8,39 +8,15 @@ from components.model_trainer import create_trainer
 from components.model_evaluator_and_pusher import create_evaluator_and_pusher
 
 def create_pipeline(pipeline_name: str, pipeline_root: str, data_path: str, serving_model_dir: str, module_file: str, project: str, region: str):
-    query = """
-    SELECT
-        CAST(trip_seconds AS BIGNUMERIC) AS TripSeconds,
-        CAST(trip_miles AS BIGNUMERIC) AS TripMiles,
-        CAST(pickup_community_area AS STRING) AS PickupCommunityArea,
-        CAST(dropoff_community_area AS STRING) AS DropoffCommunityArea,
-        CAST(trip_start_timestamp AS STRING) AS TripStartTimestamp,
-        CAST(trip_end_timestamp AS STRING) AS TripEndTimestamp,
-        CAST(payment_type AS STRING) AS PaymentType,
-        CAST(company AS STRING) AS Company
-    FROM
-        `bigquery-public-data.chicago_taxi_trips.taxi_trips`
-    WHERE trip_seconds IS NOT NULL
-       AND trip_miles IS NOT NULL
-       AND pickup_community_area IS NOT NULL
-       AND dropoff_community_area IS NOT NULL
-       AND trip_start_timestamp IS NOT NULL
-       AND trip_end_timestamp IS NOT NULL
-       AND payment_type IS NOT NULL
-       AND company IS NOT NULL
-       AND trip_seconds != 0
-       AND trip_miles != 0
-       AND pickup_community_area != 0
-       AND dropoff_community_area != 0;
-    """
-    example_gen = create_example_gen(query)
+    example_gen = create_example_gen(data_path)
     statistics_gen, schema_gen, example_validator = create_data_validation(example_gen)
     transform = create_transform(example_gen, schema_gen)
     trainer = create_trainer(transform, schema_gen, module_file)
 
-    # Use the modified function
+    # Use the modified function, which checks for previous models
     evaluator, pusher, resolver = create_evaluator_and_pusher(example_gen, trainer, serving_model_dir)
 
+    # Pipeline components list handle None values
     components = [
         example_gen,
         statistics_gen,
@@ -51,6 +27,7 @@ def create_pipeline(pipeline_name: str, pipeline_root: str, data_path: str, serv
         pusher
     ]
 
+    # Optional components
     if resolver is not None:
         components.append(resolver)
     if evaluator is not None:
